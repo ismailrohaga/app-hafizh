42bbe6f3bbb5735d553785d538769d842e07ef2a
devmail <azh.zhafir@gmail.com>
2023-03-14 19:29:54 +0700

    feat(core): add dio implementation

--- lib/core/di/dependency_injection.dart
+++ lib/core/di/dependency_injection.dart
@@ -1,5 +1,7 @@
+import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
 import 'package:hafizh/core/credentials.dart';
+import 'package:hafizh/core/network/dio_handler.dart';
 
 final locator = GetIt.instance;
 
@@ -7,7 +9,13 @@ class DependencyInjection {
   static Future<void> registerDependencies({
     required Environment env,
   }) async {
-    locator
-      ..registerSingleton<Credentials>(credentials[env]!);
+    locator.registerSingleton<Credentials>(credentials[env]!);
+    registerCoreModule();
+  }
+
+  static registerCoreModule() {
+    locator.registerLazySingleton<Dio>(() => locator<DioHandler>().dio);
+    locator.registerLazySingleton<DioHandler>(
+        () => DioHandler(baseUrl: 'https://jsonplaceholder.typicode.com'));
   }
 }
