f2148fccde8dda6b970dd66e1fb127cff6b72122
devmail <azh.zhafir@gmail.com>
2023-03-14 19:16:08 +0700

    feat(common): add various codes

---
+++ lib/common/const/named_routes.dart
@@ -0,0 +1,9 @@
+class NamedRoutes {
+  const NamedRoutes();
+
+  static const splashView = 'splashView';
+  static const onBoardingView = 'onBoardingView';
+  static const homeView = 'homeView';
+  static const detailView = 'detailView';
+  static const bookmarkView = 'bookmarkView';
+}
--- lib/common/ui/app_colors.dart
+++ lib/common/ui/app_colors.dart
@@ -1,17 +1,36 @@
 import 'dart:ui';
 
-class AppColors {
-  // light theme
-  static const Color teaGreen = Color(0xFFD6FFB7);
-  static const Color canary = Color(0xFFF5FF90);
-  static const Color maximumYellowRed = Color(0xFFFFC15E);
-  static const Color orangePeel = Color(0xFFFF9F1C);
-  static const Color midnightBlue = Color(0xFF080357);
+import 'package:flutter/material.dart';
 
-  // dark theme
-  static const Color virdianGreen = Color(0xFF379392);
-  static const Color phthaloGreen = Color(0xFF17301C);
-  static const Color steelBlue = Color(0xFF4F86C6);
-  static const Color blackChocolate = Color(0xFF28231C);
-  static const Color darkLiverHorses = Color(0xFF513B3C);
+class AppColors {
+  static const Color kPurplePrimary =
+      Color.fromRGBO(134, 62, 213, 1.0); //#863ED5
+  static const Color kPurpleSecondary =
+      Color.fromRGBO(103, 44, 188, 1.0); //#672CBC
+  static const Color kDarkPurple = Color.fromRGBO(59, 30, 119, 1.0); //#3B1E77
+  static const Color kBlackPurple = Color.fromRGBO(36, 15, 79, 1.0); //#240F4F
+  static const Color kGrey = Color.fromRGBO(135, 137, 163, 1.0); //#8789A3
+  static const Color kGrey92 = Color.fromRGBO(235, 235, 235, 1.0); //#ebebeb
+  static const Color kGreyLight = Color.fromRGBO(171, 175, 215, 1.0); //#abafd7
+  static const Color kMikadoYellow =
+      Color.fromRGBO(249, 176, 145, 1.0); //#F9B091
+  static const Color kLinearPurple1 = Color.fromRGBO(223, 152, 250, 1.0);
+  static const Color kLinearPurple2 = Color.fromRGBO(144, 85, 255, 1.0);
+  static const Color kDarkTheme = Color.fromRGBO(9, 25, 69, 1.0); //#152451
+
+  static const kColorScheme = ColorScheme(
+    primary: kPurplePrimary,
+    primaryContainer: kPurplePrimary,
+    secondary: kPurpleSecondary,
+    secondaryContainer: kPurpleSecondary,
+    surface: kPurplePrimary,
+    background: kPurplePrimary,
+    error: Colors.red,
+    onPrimary: kDarkPurple,
+    onSecondary: Colors.white,
+    onSurface: Colors.white,
+    onBackground: Colors.white,
+    onError: Colors.white,
+    brightness: Brightness.light,
+  );
 }
