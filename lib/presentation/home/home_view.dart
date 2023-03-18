4743477efc2fbdaa4e40f40e84dc4a660a5bc90a
Rachmizard <rachmizard11072000@gmail.com>
2023-03-19 01:07:06 +0700

    feat(): add atom horizontal percentage indicator widget

--- lib/presentation/home/home_view.dart
+++ lib/presentation/home/home_view.dart
@@ -1,15 +1,32 @@
 import 'package:flutter/material.dart';
 import 'package:hafizh/common/ui/widget/atoms/circular_percentage_indicator_widget.dart';
+import 'package:hafizh/common/ui/widget/atoms/horizontal_progress_bar_indicator_widget.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Center(
-        child: AtomCircularPercentageIndicatorWidget(
-          value: 0.8,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const [
+            AtomCircularPercentageIndicatorWidget(
+              value: 0.8,
+            ),
+            SizedBox(height: 24.0),
+            Padding(
+              padding: EdgeInsets.symmetric(
+                horizontal: 16.0,
+              ),
+              child: AtomHorizontalProgressBarIndicator(
+                count: 3,
+                value: 2,
+              ),
+            ),
+          ],
         ),
       ),
     );
