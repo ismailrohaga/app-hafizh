7cf882d14847467f4cfcf9559896321c60c41e92
Rachmizard <rachmizard11072000@gmail.com>
2023-03-18 19:59:56 +0700

    fix(): add safe area on quran view

--- lib/presentation/quran/quran_view.dart
+++ lib/presentation/quran/quran_view.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-import 'package:hafizh/common/const/screen_padding_constant.dart';
 import 'package:hafizh/common/const/named_routes.dart';
+import 'package:hafizh/common/const/screen_padding_constant.dart';
 import 'package:hafizh/common/ui/widget/molecules/card/surah_card_widget.dart';
 
 class QuranView extends StatelessWidget {
@@ -9,18 +9,21 @@ class QuranView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.symmetric(
-            horizontal: ScreenPaddingConstant.horizontal),
-        child: ListView.builder(
-          scrollDirection: Axis.vertical,
-          itemCount: 114,
-          itemBuilder: (context, index) => MoleculeSurahCardWidget(
-            subtitle: "Last Read at ayah ${index + 1}",
-            name: "Al-Fatiha",
-            number: index + 1,
-            onTap: () => context.pushNamed(NamedRoutes.detailSurahView),
+    return SafeArea(
+      child: Scaffold(
+        resizeToAvoidBottomInset: true,
+        body: Padding(
+          padding: const EdgeInsets.symmetric(
+              horizontal: ScreenPaddingConstant.horizontal),
+          child: ListView.builder(
+            scrollDirection: Axis.vertical,
+            itemCount: 114,
+            itemBuilder: (context, index) => MoleculeSurahCardWidget(
+              subtitle: "Last Read at ayah ${index + 1}",
+              name: "Al-Fatiha",
+              number: index + 1,
+              onTap: () => context.pushNamed(NamedRoutes.detailSurahView),
+            ),
           ),
         ),
       ),
