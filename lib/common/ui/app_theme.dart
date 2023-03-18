6d622171610acd258bb0b4eec169aedf6c9c0a74
devmail <azh.zhafir@gmail.com>
2023-03-18 17:28:39 +0700

    style: update app styling

--- lib/common/ui/app_theme.dart
+++ lib/common/ui/app_theme.dart
@@ -4,38 +4,47 @@ import 'package:hafizh/common/ui/app_colors.dart';
 
 class AppTheme {
   final kTextTheme = TextTheme(
+    displayLarge:
+        GoogleFonts.nunitoSans(fontSize: 57, fontWeight: FontWeight.normal),
+    displayMedium:
+        GoogleFonts.nunitoSans(fontSize: 45, fontWeight: FontWeight.normal),
+    displaySmall:
+        GoogleFonts.nunitoSans(fontSize: 36, fontWeight: FontWeight.normal),
+    headlineLarge:
+        GoogleFonts.nunitoSans(fontSize: 32, fontWeight: FontWeight.bold),
+    headlineMedium:
+        GoogleFonts.nunitoSans(fontSize: 28, fontWeight: FontWeight.bold),
     headlineSmall:
-        GoogleFonts.poppins(fontSize: 24, fontWeight: FontWeight.w400),
-    titleLarge: GoogleFonts.poppins(
-        fontSize: 18, fontWeight: FontWeight.w500, letterSpacing: 0.15),
-    titleMedium: GoogleFonts.poppins(
-        fontSize: 14, fontWeight: FontWeight.w400, letterSpacing: 0.15),
-    bodyMedium: GoogleFonts.poppins(
-        fontSize: 12, fontWeight: FontWeight.w400, letterSpacing: 0.25),
+        GoogleFonts.nunitoSans(fontSize: 24, fontWeight: FontWeight.bold),
+    titleLarge:
+        GoogleFonts.nunitoSans(fontSize: 22, fontWeight: FontWeight.bold),
+    titleMedium:
+        GoogleFonts.nunitoSans(fontSize: 16, fontWeight: FontWeight.bold),
+    titleSmall:
+        GoogleFonts.nunitoSans(fontSize: 14, fontWeight: FontWeight.bold),
+    labelLarge:
+        GoogleFonts.nunitoSans(fontSize: 14, fontWeight: FontWeight.normal),
+    labelMedium:
+        GoogleFonts.nunitoSans(fontSize: 12, fontWeight: FontWeight.bold),
+    labelSmall:
+        GoogleFonts.nunitoSans(fontSize: 11, fontWeight: FontWeight.normal),
+    bodyLarge:
+        GoogleFonts.nunitoSans(fontSize: 16, fontWeight: FontWeight.normal),
+    bodyMedium:
+        GoogleFonts.nunitoSans(fontSize: 14, fontWeight: FontWeight.normal),
+    bodySmall:
+        GoogleFonts.nunitoSans(fontSize: 12, fontWeight: FontWeight.normal),
   );
 
   ThemeData get darkTheme => ThemeData.dark().copyWith(
-        colorScheme: AppColors.kColorScheme.copyWith(
-          primary: AppColors.kDarkTheme,
-          onPrimary: Colors.white,
-        ),
-        appBarTheme: const AppBarTheme(elevation: 0),
-        scaffoldBackgroundColor: AppColors.kDarkTheme,
-        textTheme: kTextTheme.apply(
-          bodyColor: Colors.white,
-          decorationColor: Colors.white,
-          displayColor: Colors.white,
-        ),
-      );
+      colorScheme: AppColors.kColorScheme,
+      appBarTheme: const AppBarTheme(elevation: 0),
+      scaffoldBackgroundColor: Colors.black,
+      textTheme: kTextTheme);
 
   ThemeData get lightTheme => ThemeData.light().copyWith(
-        colorScheme: AppColors.kColorScheme,
-        appBarTheme: const AppBarTheme(elevation: 0),
-        scaffoldBackgroundColor: Colors.white,
-        textTheme: kTextTheme.apply(
-          bodyColor: AppColors.kPurpleSecondary,
-          decorationColor: AppColors.kPurpleSecondary,
-          displayColor: AppColors.kPurpleSecondary,
-        ),
-      );
+      colorScheme: AppColors.kColorScheme,
+      appBarTheme: const AppBarTheme(elevation: 0),
+      scaffoldBackgroundColor: Colors.white,
+      textTheme: kTextTheme);
 }
