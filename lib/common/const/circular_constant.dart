e007b848f9af1f13da48bde27d58d84302291e27
devmail <azh.zhafir@gmail.com>
2023-03-19 19:12:14 +0700

    refactor: add doc comment

--- lib/common/const/circular_constant.dart
+++ lib/common/const/circular_constant.dart
@@ -1,7 +1,16 @@
 class CircularConstant {
+  /// 4.0
   static const double sm = 4.0;
+
+  /// 8.0
   static const double md = 8.0;
+
+  /// 12.0
   static const double lg = 12.0;
+
+  /// 16.0
   static const double xl = 16.0;
+
+  /// 24.0
   static const double xxl = 24.0;
 }
