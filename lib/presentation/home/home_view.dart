0c6448038f3a87c23a534164819470a3fcf31f55
devmail <azh.zhafir@gmail.com>
2023-03-20 23:58:44 +0700

    feat(home-hero): minimum ui impl

--- lib/presentation/home/home_view.dart
+++ lib/presentation/home/home_view.dart
@@ -1,13 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:hafizh/common/const/screen_padding_constant.dart';
-import 'package:hafizh/common/const/spacing_constant.dart';
 import 'package:hafizh/common/ext/build_context_ext.dart';
 import 'package:hafizh/common/ui/widget/atoms/circular_percentage_indicator_widget.dart';
 import 'package:hafizh/common/ui/widget/atoms/horizontal_progress_bar_indicator_widget.dart';
 import 'package:hafizh/common/ui/widget/molecules/button/hafizh_button_widget.dart';
 import 'package:hafizh/common/ui/widget/molecules/card/surah_card_widget.dart';
-import 'package:hafizh/common/ui/widget/molecules/chart/bar_chart_widget.dart';
+import 'package:hafizh/common/ui/widget/molecules/tile/surah_tile_title_widget.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
@@ -15,85 +13,110 @@ class HomeView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: context.colors.background,
-      body: AnnotatedRegion<SystemUiOverlayStyle>(
-        value: SystemUiOverlayStyle.light,
-        child: SafeArea(
-          child: Column(
-            children: [
-              Container(
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(
-                      horizontal: ScreenPaddingConstant.horizontal,
-                      vertical: SpacingConstant.lg),
-                  child: Column(
+      body: CustomScrollView(slivers: [
+        SliverAppBar(
+          expandedHeight: 320,
+          backgroundColor: context.colors.background,
+          flexibleSpace: FlexibleSpaceBar(
+            background: Container(
+              padding: const EdgeInsets.symmetric(
+                  vertical: 42, horizontal: ScreenPaddingConstant.horizontal),
+              decoration: BoxDecoration(
+                  image: DecorationImage(
+                      image: AssetImage("assets/images/home_hero.png"),
+                      fit: BoxFit.cover)),
+              child: Column(
+                children: [
+                  Expanded(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: [
+                        Expanded(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            children: [
+                              Text(
+                                'Hi Bayek',
+                                style: context.textTheme.headlineMedium,
+                              ),
+                              SizedBox(
+                                height: 20,
+                              ),
+                              MoleculeSurahTileTitleWidget(
+                                name: 'Al-Mulk',
+                                number: 92,
+                              ),
+                              SizedBox(
+                                height: 12,
+                              ),
+                              Text(
+                                'Today’s Tahfidz. Ayah 16',
+                                style: context.textTheme.labelLarge,
+                              ),
+                              SizedBox(
+                                height: 8,
+                              ),
+                              AtomHorizontalProgressBarIndicator(
+                                count: 3,
+                                value: 2,
+                              ),
+                              SizedBox(
+                                height: 8,
+                              ),
+                              Text(
+                                'Great progress! You\'ll finish memorizing al-mulk in 5 days at this rate!',
+                                style: context.textTheme.labelSmall,
+                              ),
+                            ],
+                          ),
+                        ),
+                        SizedBox(
+                          width: 12,
+                        ),
+                        const AtomCircularPercentageIndicatorWidget(
+                          value: 0.8,
+                        )
+                      ],
+                    ),
+                  ),
+                  SizedBox(
+                    height: 22,
+                  ),
+                  Row(
                     children: [
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Expanded(
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text('Hi Bayek'),
-                                Text('Hi Bayek'),
-                                AtomHorizontalProgressBarIndicator(
-                                  count: 3,
-                                  value: 2,
-                                ),
-                                Text('Hi Bayek')
-                              ],
-                            ),
-                          ),
-                          const AtomCircularPercentageIndicatorWidget(
-                            value: 0.8,
-                          )
-                        ],
-                      ),
-                      Row(
-                        children: [
-                          Expanded(
-                            child: HafizhButtonWidget(
-                              text: 'Start New Tahfidz',
-                            ),
-                          ),
-                        ],
+                      Expanded(
+                        child: HafizhButtonWidget(
+                          text: 'Start New Tahfidz',
+                        ),
                       ),
                     ],
                   ),
-                ),
+                ],
               ),
-              Container(
-                color: Colors.white,
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(
-                      horizontal: ScreenPaddingConstant.horizontal),
-                  child: Column(children: [
-                    MoleculeBarChartWidget(
-                      dataSource: [
-                        ChartData('10 Days', 2),
-                        ChartData('6 Days', 3),
-                        ChartData('4 Days', 4),
-                        ChartData('2 Days', 5),
-                        ChartData('1 Days', 6),
-                      ],
-                    ),
-                    const SizedBox(
-                      height: 33,
-                    ),
-                    MoleculeSurahCardWidget(
-                      subtitle: "Last Read at ayah 122",
-                      name: "Al-Fatiha",
-                      number: 1,
-                      onTap: () => {},
-                    ),
-                  ]),
-                ),
-              ),
-            ],
+            ),
           ),
         ),
-      ),
+        SliverList(
+          delegate: SliverChildListDelegate([
+            Padding(
+              padding: const EdgeInsets.symmetric(
+                  vertical: 20, horizontal: ScreenPaddingConstant.horizontal),
+              child: Column(
+                children: [
+                  MoleculeSurahCardWidget(
+                    subtitle: "Last Read at ayah 122",
+                    name: "Al-Fatiha",
+                    number: 1,
+                    onTap: () => {},
+                  ),
+                ],
+              ),
+            ),
+          ]),
+        ),
+      ]),
     );
   }
 }
