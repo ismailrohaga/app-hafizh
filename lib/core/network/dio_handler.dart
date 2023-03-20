d1b2c6fe529b710f7370c3c59b8e2d798389e130
Rachmizard <rachmizard11072000@gmail.com>
2023-03-21 01:07:32 +0700

    feat: add dio http cache interceptors

--- lib/core/network/dio_handler.dart
+++ lib/core/network/dio_handler.dart
@@ -1,5 +1,8 @@
 import 'package:dio/dio.dart';
+import 'package:dio_cache_interceptor/dio_cache_interceptor.dart';
+
 import 'package:hafizh/core/network/api_interceptors.dart';
+import 'package:hafizh/core/network/cache_interceptors.dart';
 
 class DioHandler {
   final String baseUrl;
@@ -12,9 +15,35 @@ class DioHandler {
       connectTimeout: const Duration(seconds: 50),
       receiveTimeout: const Duration(seconds: 30),
     );
+
+    CacheOptions cacheOptions = CacheOptions(
+      store: MemCacheStore(),
+      // All subsequent fields are optional.
+      // Default.
+      policy: CachePolicy.request,
+      // Returns a cached response on error but for statuses 401 & 403.
+      // Also allows to return a cached response on network errors (e.g. offline usage).
+      // Defaults to [null].
+      hitCacheOnErrorExcept: [401, 403],
+      // Overrides any HTTP directive to delete entry past this duration.
+      // Useful only when origin server has no cache config or custom behaviour is desired.
+      // Defaults to [null].
+      maxStale: const Duration(hours: 1),
+      // Default. Allows 3 cache sets and ease cleanup.
+      priority: CachePriority.normal,
+      // Default. Body and headers encryption with your own algorithm.
+      cipher: null,
+      // Default. Key builder to retrieve requests.
+      keyBuilder: CacheOptions.defaultCacheKeyBuilder,
+      // Default. Allows to cache POST requests.
+      // Overriding [keyBuilder] is strongly recommended when [true].
+      allowPostMethod: false,
+    );
+
     options.headers = _defaultHeader();
     final dio = Dio(options);
     dio.interceptors.add(ApiInterceptors());
+    dio.interceptors.add(CacheInterceptor(options: cacheOptions));
 
     return dio;
   }
