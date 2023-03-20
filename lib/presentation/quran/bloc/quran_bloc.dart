cb2bc17388b86835925f874b9f8baa061713dd76
Rachmizard <rachmizard11072000@gmail.com>
2023-03-21 03:17:51 +0700

    feat: add a filter ability for quran bloc

--- lib/presentation/quran/bloc/quran_bloc.dart
+++ lib/presentation/quran/bloc/quran_bloc.dart
@@ -9,11 +9,13 @@ class QuranBloc extends Bloc<QuranEvent, QuranState> {
   final GetSurahUsecase getSurahUsecase;
 
   QuranBloc({required this.getSurahUsecase})
-      : super(QuranState(statusSurah: ViewData.initial())) {
-    on<FetchSurah>(fetchSurah);
+      : super(QuranState(
+            statusSurah: ViewData.initial(), filteredSurah: const [])) {
+    on<FetchSurah>(_fetchSurah);
+    on<FilterSurah>(_filterSurah);
   }
 
-  void fetchSurah(FetchSurah event, Emitter<QuranState> emit) async {
+  void _fetchSurah(FetchSurah event, Emitter<QuranState> emit) async {
     try {
       emit(state.copyWith(
         statusSurah: ViewData.loading(message: 'Loading Fetch Surah...'),
@@ -28,12 +30,37 @@ class QuranBloc extends Bloc<QuranEvent, QuranState> {
         response.fold(
             (failure) => emit(state.copyWith(
                 statusSurah: ViewData.error(message: failure.message))),
-            (data) =>
-                emit(state.copyWith(statusSurah: ViewData.loaded(data: data))));
+            (data) => emit(state.copyWith(
+                statusSurah: ViewData.loaded(data: data),
+                filteredSurah: data)));
       }
     } catch (e) {
       emit(state.copyWith(
           statusSurah: ViewData.error(message: "Error Fetch Surah: $e")));
+    }
+  }
+
+  void _filterSurah(FilterSurah event, Emitter<QuranState> emit) async {
+    emit(state.copyWith(query: event.query));
+
+    final query = event.query;
+    final surah = state.statusSurah.data;
+
+    if (query.isEmpty) {
+      emit(state.copyWith(filteredSurah: surah));
+    } else {
+      final filteredSurah = surah!.where((surah) {
+        final surahName = surah.name.transliteration.id.toLowerCase();
+        final surahNameTranslate = surah.numberOfVerses.toString();
+        final surahNumber = surah.number.toString();
+        final queryLowerCase = query.toLowerCase();
+
+        return surahName.contains(queryLowerCase) ||
+            surahNameTranslate.contains(queryLowerCase) ||
+            surahNumber.contains(queryLowerCase);
+      }).toList();
+
+      emit(state.copyWith(filteredSurah: filteredSurah));
     }
   }
 }
--- lib/presentation/quran/bloc/quran_state.dart
+++ lib/presentation/quran/bloc/quran_state.dart
@@ -4,13 +4,22 @@ import 'package:hafizh/domain/entity/surah_entity.dart';
 
 class QuranState extends Equatable {
   final ViewData<List<SurahEntity>> statusSurah;
+  final List<SurahEntity> filteredSurah;
+  final String? query;
 
-  const QuranState({required this.statusSurah});
+  const QuranState(
+      {required this.statusSurah, this.filteredSurah = const [], this.query});
 
-  QuranState copyWith({ViewData<List<SurahEntity>>? statusSurah}) {
-    return QuranState(statusSurah: statusSurah ?? this.statusSurah);
+  QuranState copyWith(
+      {ViewData<List<SurahEntity>>? statusSurah,
+      List<SurahEntity>? filteredSurah,
+      String? query}) {
+    return QuranState(
+        statusSurah: statusSurah ?? this.statusSurah,
+        filteredSurah: filteredSurah ?? this.filteredSurah,
+        query: query ?? this.query);
   }
 
   @override
-  List<Object?> get props => [statusSurah];
+  List<Object?> get props => [statusSurah, filteredSurah, query];
 }
