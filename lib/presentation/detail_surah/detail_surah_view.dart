305464c8b5c318a9c8c2ea51e3b2d198665ecad9
Rachmizard <rachmizard11072000@gmail.com>
2023-03-21 22:13:28 +0700

    feat: implement detail surah functionality

--- lib/presentation/detail_surah/detail_surah_view.dart
+++ lib/presentation/detail_surah/detail_surah_view.dart
@@ -1,20 +1,65 @@
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
 import 'package:hafizh/common/const/named_routes.dart';
+import 'package:hafizh/common/dependencies/dependencies.dart';
+import 'package:hafizh/common/ext/build_context_ext.dart';
+import 'package:hafizh/common/state/view_data_state.dart';
 import 'package:hafizh/common/ui/widget/organisms/appbar/scaffold_with_app_bar.dart';
+import 'package:hafizh/presentation/detail_surah/bloc/detail_surah_bloc.dart';
+import 'package:hafizh/presentation/detail_surah/bloc/detail_surah_event.dart';
+import 'package:hafizh/presentation/detail_surah/bloc/detail_surah_state.dart';
+import 'package:hafizh/presentation/detail_surah/widgets/list_verse_widget.dart';
 
-class DetailSurahView extends StatelessWidget {
-  const DetailSurahView({super.key});
+class DetailSurahView extends StatefulWidget {
+  final int number;
+  const DetailSurahView({super.key, required this.number});
+
+  @override
+  State<DetailSurahView> createState() => _DetailSurahViewState();
+}
+
+class _DetailSurahViewState extends State<DetailSurahView> {
+  @override
+  void initState() {
+    Future.microtask(() {
+      _fetchDetailSurah();
+    });
+
+    super.initState();
+  }
+
+  _fetchDetailSurah() {
+    context.read<DetailSurahBloc>().add(FetchDetailSurah(widget.number));
+  }
 
   @override
   Widget build(BuildContext context) {
-    return OrganismScaffoldWithDetailSurahAppBar(
-        surah: "Surah Al-Baqarah",
-        verse: 1,
-        surahNumber: 2,
-        onBackPressed: () => context.canPop()
-            ? context.pop()
-            : context.goNamed(NamedRoutes.quranView),
-        child: const Center(child: Text("DetailSurah View")));
+    final textTheme = context.textTheme;
+    final colors = context.colors;
+
+    return BlocConsumer<DetailSurahBloc, DetailSurahState>(
+      listener: (context, state) {},
+      builder: (context, state) {
+        final status = state.statusSurahDetail.status;
+        final surah = state.statusSurahDetail.data;
+        final verses = surah?.verses;
+
+        return OrganismScaffoldWithDetailSurahAppBar(
+            loading: status.isLoading,
+            surah: surah?.name.transliteration.id,
+            verse: surah?.numberOfVerses,
+            surahNumber: surah?.number,
+            onBackPressed: () => context.goNamed(NamedRoutes.quranView),
+            child: RefreshIndicator(
+              onRefresh: () async {
+                _fetchDetailSurah();
+              },
+              child: ListVerseWidget(
+                  verses: verses,
+                  status: status,
+                  textTheme: textTheme,
+                  colors: colors),
+            ));
+      },
+    );
   }
 }
