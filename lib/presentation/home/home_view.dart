9e3464d75dcc95eebf29ae6afa1e6f19f32ed3c2
Rachmizard <rachmizard11072000@gmail.com>
2023-03-23 17:07:19 +0700

    fix: ui implementation sliver in home view

--- lib/presentation/home/home_view.dart
+++ lib/presentation/home/home_view.dart
@@ -1,37 +1,45 @@
 import 'package:flutter/material.dart';
 import 'package:hafizh/common/const/const.dart';
+import 'package:hafizh/common/ext/build_context_ext.dart';
 
 import 'package:hafizh/common/ui/widget/molecules/molecules.dart';
 import 'package:hafizh/common/ui/widget/organisms/organisms.dart';
 
 import 'package:hafizh/common/dependencies/dependencies.dart';
 import 'package:hafizh/presentation/bloc/auth/auth_bloc.dart';
-
-import 'package:hafizh/presentation/home/widgets/tahfidz_history_card_widget.dart';
-import 'package:hafizh/presentation/home/widgets/tahfidz_history_list_delegate_widget.dart';
+import 'package:hafizh/presentation/home/widgets/sliver_tahfidz_history_card_widget.dart';
+import 'package:hafizh/presentation/home/widgets/sliver_tahfidz_history_list_delegate_widget.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final authBloc = context.select((AuthBloc bloc) => bloc);
+
     return BlocListener<AuthBloc, AuthState>(
         listener: (context, state) {
           if (state.status == AuthStatus.unauthenticated) {
             context.goNamed(NamedRoutes.loginView);
           }
         },
-        child: const Scaffold(
+        child: Scaffold(
           body: SliverScrollWrapperViewWidget(
-            background: DashboardWidget(
+            expandedHeight: 320,
+            title: Text(
+              'Hi ${authBloc.state.user.name}',
+              style: context.textTheme.titleLarge,
+            ),
+            background: const DashboardWidget(
               backgroundImage: AssetConstant.heroBgImage,
+              contractWidget: DashboardFullContractWidget(),
               callToAction: HafizhButtonWidget(
                 text: 'Start New Tahfidz',
               ),
             ),
-            listDelegate: [
-              TahfidzHistoryCardWidget(),
-              TahfidzhHistoryListDelegateWidget()
+            slivers: const [
+              SliverTahfidzHistoryCardWidget(),
+              SliverTahfidzhHistoryListDelegateWidget()
             ],
           ),
         ));
---
+++ lib/presentation/home/widgets/sliver_tahfidz_history_list_delegate_widget.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:hafizh/common/const/const.dart';
+import 'package:hafizh/common/ui/widget/molecules/card/surah_card_widget.dart';
+
+class SliverTahfidzhHistoryListDelegateWidget extends StatelessWidget {
+  const SliverTahfidzhHistoryListDelegateWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverList(
+      delegate: SliverChildBuilderDelegate((context, index) {
+        return Padding(
+          padding: const EdgeInsets.symmetric(
+              horizontal: ScreenPaddingConstant.horizontal),
+          child:
+              Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+            const SizedBox(
+              height: SpacingConstant.sm,
+            ),
+            SurahCardWidget(
+              subtitle: "Last Read at ayah 122",
+              name: "Al-Fatiha",
+              number: 1,
+              onTap: () => {},
+            ),
+          ]),
+        );
+      }, childCount: 10),
+    );
+  }
+}
