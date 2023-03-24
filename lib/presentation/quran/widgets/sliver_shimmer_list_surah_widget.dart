f95ae4cde747551b2d9451f13cc37f8b6c6d0a77
Rachmizard <rachmizard11072000@gmail.com>
2023-03-24 14:21:12 +0700

    Feat/show up animation (#24)
    * chore: setup android firebase sdk with env
    
    * chore: update google-services.json for prod
    
    * fix: add physics custom scroll view for force android scroll behavior
    
    * fix: add centerTitle app bar for android support
    
    * feat: implement show up animation to each view

--- lib/presentation/quran/widgets/sliver_shimmer_list_surah_widget.dart
+++ lib/presentation/quran/widgets/sliver_shimmer_list_surah_widget.dart
@@ -10,25 +10,29 @@ class SliverShimmerListSurahWidget extends StatelessWidget {
     return SliverList(
         delegate: SliverChildBuilderDelegate(
             childCount: 6,
-            (context, index) => Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const SizedBox(height: SpacingConstant.sm),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(
-                          horizontal: ScreenPaddingConstant.horizontal),
-                      child: Shimmer.fromColors(
-                        baseColor: Colors.grey[300]!,
-                        highlightColor: Colors.grey[100]!,
-                        enabled: true,
-                        child: Container(
-                          width: double.infinity,
-                          height: 110,
-                          color: Colors.white,
+            (context, index) => ShowUpAnimation(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const SizedBox(height: SpacingConstant.sm),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: ScreenPaddingConstant.horizontal),
+                        child: Shimmer.fromColors(
+                          baseColor: Colors.grey[300]!,
+                          highlightColor: Colors.grey[100]!,
+                          enabled: true,
+                          child: Container(
+                              width: double.infinity,
+                              height: 110,
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(
+                                      CircularConstant.lg),
+                                  color: Colors.white)),
                         ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 )));
   }
 }
