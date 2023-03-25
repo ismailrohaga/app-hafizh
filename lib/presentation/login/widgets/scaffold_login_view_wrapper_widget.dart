9c1e44e5f6f2458b0850403760bcd58afaa8f897
Rachmizard <rachmizard11072000@gmail.com>
2023-03-25 21:32:52 +0700

    fix: adjust scaffold login view wrapper, etc

--- lib/presentation/login/widgets/scaffold_login_view_wrapper_widget.dart
+++ lib/presentation/login/widgets/scaffold_login_view_wrapper_widget.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:hafizh/common/const/const.dart';
 import 'package:hafizh/common/dependencies/dependencies.dart';
+import 'package:hafizh/common/ext/build_context_ext.dart';
 import 'package:hafizh/common/provider/preference_settings_provider.dart';
+import 'package:hafizh/common/ui/app_colors.dart';
 
 class ScaffoldLoginViewWrapperWidget extends StatelessWidget {
   final Widget child;
@@ -24,6 +26,33 @@ class ScaffoldLoginViewWrapperWidget extends StatelessWidget {
                       vertical: ScreenPaddingConstant.vertical,
                       horizontal: ScreenPaddingConstant.horizontal,
                     ),
-                    child: child))));
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        child,
+                        RichText(
+                          text: TextSpan(
+                            text: "Don't have an account? ",
+                            style: context.textTheme.labelMedium?.copyWith(
+                              color: prefsSettingsProvider.isDarkTheme
+                                  ? Colors.grey[300]
+                                  : Colors.black,
+                            ),
+                            children: [
+                              TextSpan(
+                                text: "Sign Up",
+                                style: context.textTheme.labelMedium?.copyWith(
+                                  color: prefsSettingsProvider.isDarkTheme
+                                      ? Colors.blue
+                                      : AppColors.kNormalBlue,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    )))));
   }
 }
