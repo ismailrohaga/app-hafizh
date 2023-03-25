92ff58e78b40175aa7e64310129ba8e4c79cc0d0
Rachmizard <rachmizard11072000@gmail.com>
2023-03-25 15:51:47 +0700

    feat: ui implementation sign in options

--- lib/presentation/login/login_view.dart
+++ lib/presentation/login/login_view.dart
@@ -1,104 +1,160 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 import 'package:hafizh/common/const/const.dart';
 import 'package:hafizh/common/dependencies/dependencies.dart';
 import 'package:hafizh/common/ext/build_context_ext.dart';
+import 'package:hafizh/common/provider/preference_settings_provider.dart';
 import 'package:hafizh/common/state/view_data_state.dart';
+import 'package:hafizh/common/ui/app_colors.dart';
+import 'package:hafizh/common/ui/widget/molecules/molecules.dart';
 import 'package:hafizh/presentation/login/cubit/login_cubit.dart';
 import 'package:hafizh/presentation/login/cubit/login_state.dart';
-import 'package:hafizh/presentation/login/google_signin_button.dart';
+import 'package:hafizh/presentation/login/widgets/google_signin_button.dart';
 
 class LoginView extends StatelessWidget {
   const LoginView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: AnnotatedRegion<SystemUiOverlayStyle>(
-        value: SystemUiOverlayStyle.light,
-        child: Container(
-          decoration: BoxDecoration(
-            color: context.colors.background,
-          ),
+    var prefsSettingsProvider = context.read<PreferenceSettingsProvider>();
+
+    var googleSignInBloc = BlocConsumer<LoginCubit, LoginState>(
+      bloc: context.read<LoginCubit>(),
+      listener: (context, state) {
+        final status = state.viewData.status;
+        final message = state.message;
+
+        if (status.isError) {
+          context.scaffoldMessenger.showSnackBar(
+            SnackBar(
+              backgroundColor: context.colors.primary,
+              content: Text(message.toString()),
+            ),
+          );
+        }
+
+        if (status.isHasData) {
+          context.scaffoldMessenger.showSnackBar(
+            SnackBar(
+              backgroundColor: context.colors.background,
+              content: Text(message.toString()),
+            ),
+          );
+
+          context.goNamed(NamedRoutes.homeView);
+        }
+      },
+      builder: (context, state) {
+        final loading = state.viewData.status.isLoading;
+
+        return GoogleSignInButton(
+          loading: loading,
+          onPressed: () => context.read<LoginCubit>().signInWithGoogle(),
+        );
+      },
+    );
+
+    return AnnotatedRegion<SystemUiOverlayStyle>(
+      value: prefsSettingsProvider.isDarkTheme
+          ? SystemUiOverlayStyle.light
+          : SystemUiOverlayStyle.dark,
+      child: Scaffold(
+        body: SafeArea(
           child: Padding(
             padding: const EdgeInsets.symmetric(
-              vertical: 40.0,
+              vertical: ScreenPaddingConstant.vertical,
+              horizontal: ScreenPaddingConstant.horizontal,
             ),
             child: Column(
-              mainAxisAlignment: MainAxisAlignment.end,
-              crossAxisAlignment: CrossAxisAlignment.stretch,
+              crossAxisAlignment: CrossAxisAlignment.center,
               children: <Widget>[
-                Padding(
-                  padding: const EdgeInsets.all(48.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: <Widget>[
-                      const Center(
-                        child: Image(
-                          image: AssetImage(
-                            AssetConstant.onBoardImage,
-                          ),
-                          height: 300.0,
-                          width: 300.0,
-                        ),
-                      ),
-                      const SizedBox(height: 16.0),
-                      Text(
-                        'Login',
-                        style: context.textTheme.headlineMedium,
-                        textAlign: TextAlign.center,
-                      ),
-                      const SizedBox(height: 16.0),
-                      Text(
-                        'Description',
-                        style: context.textTheme.bodyLarge,
-                        textAlign: TextAlign.center,
-                      ),
-                    ],
+                Text(
+                  'Sign In',
+                  style: context.textTheme.headlineLarge?.copyWith(
+                      color: prefsSettingsProvider.isDarkTheme
+                          ? Colors.grey[300]
+                          : Colors.black),
+                  textAlign: TextAlign.center,
+                ),
+                const SizedBox(height: SpacingConstant.xl),
+                googleSignInBloc,
+                const SizedBox(height: SpacingConstant.md),
+                HafizhButtonWidget(
+                  text: 'Sign-in with Email',
+                  onTap: () {
+                    prefsSettingsProvider
+                        .enableDarkTheme(!prefsSettingsProvider.isDarkTheme);
+                  },
+                  leftIcon: const Icon(
+                    Icons.email,
+                    color: Colors.white,
                   ),
                 ),
-                const SizedBox(height: 16.0),
-                BlocConsumer<LoginCubit, LoginState>(
-                  bloc: context.read<LoginCubit>(),
-                  listener: (context, state) {
-                    final status = state.viewData.status;
-                    final message = state.message;
-
-                    if (status.isError) {
-                      context.scaffoldMessenger.showSnackBar(
-                        SnackBar(
-                          backgroundColor: context.colors.primary,
-                          content: Text(message.toString()),
-                        ),
-                      );
-                    }
-
-                    if (status.isHasData) {
-                      context.scaffoldMessenger.showSnackBar(
-                        SnackBar(
-                          backgroundColor: context.colors.background,
-                          content: Text(message.toString()),
-                        ),
-                      );
-
-                      context.goNamed(NamedRoutes.homeView);
-                    }
-                  },
-                  builder: (context, state) {
-                    final loading = state.viewData.status.isLoading;
-
-                    return GoogleSignInButton(
-                      loading: loading,
-                      onPressed: () =>
-                          context.read<LoginCubit>().signInWithGoogle(),
-                    );
-                  },
-                ),
+                const SizedBox(height: SpacingConstant.xl),
+                const TermOfServicePrivacyPolicyTextWidget(),
               ],
             ),
           ),
         ),
+      ),
+    );
+  }
+}
+
+class TermOfServicePrivacyPolicyTextWidget extends StatelessWidget {
+  const TermOfServicePrivacyPolicyTextWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final prefsSettingsProvider = context.read<PreferenceSettingsProvider>();
+
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 52),
+      child: Text.rich(
+        style: context.textTheme.bodyMedium?.copyWith(
+          color: prefsSettingsProvider.isDarkTheme
+              ? Colors.grey[300]
+              : AppColors.kDeepGreen,
+        ),
+        TextSpan(
+          text: 'By signing up you are agreeing to our ',
+          children: [
+            TextSpan(
+              text: 'Terms of Service',
+              recognizer: TapGestureRecognizer()
+                ..onTap = () {
+                  // TODO: implement onTap for Terms of Service
+                },
+              style: context.textTheme.bodyMedium?.copyWith(
+                color: prefsSettingsProvider.isDarkTheme
+                    ? Colors.blueAccent
+                    : AppColors.kNormalBlue,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const TextSpan(
+              text: ' and ',
+            ),
+            TextSpan(
+              text: 'Privacy Policy',
+              recognizer: TapGestureRecognizer()
+                ..onTap = () {
+                  // TODO: implement onTap for Privacy Policy
+                },
+              style: context.textTheme.bodyMedium?.copyWith(
+                color: prefsSettingsProvider.isDarkTheme
+                    ? Colors.blueAccent
+                    : AppColors.kNormalBlue,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
+        ),
+        textAlign: TextAlign.center,
       ),
     );
   }
