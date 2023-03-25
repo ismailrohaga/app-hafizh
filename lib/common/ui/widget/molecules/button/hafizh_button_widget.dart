abc09ed246e8427b8f1aef76a5fd15e04ba95a25
Rachmizard <rachmizard11072000@gmail.com>
2023-03-26 02:45:31 +0700

    feat: add ability disabled UI state for hafizh button

--- lib/common/ui/widget/molecules/button/hafizh_button_widget.dart
+++ lib/common/ui/widget/molecules/button/hafizh_button_widget.dart
@@ -1,33 +1,44 @@
 import 'package:flutter/material.dart';
 import 'package:hafizh/common/const/const.dart';
+import 'package:hafizh/common/dependencies/dependencies.dart';
 import 'package:hafizh/common/ext/build_context_ext.dart';
 
 class HafizhButtonWidget extends StatelessWidget {
   final String text;
   final Icon? leftIcon;
   final Icon? rightIcon;
+  final bool disabled;
   final void Function() onTap;
 
-  const HafizhButtonWidget(
-      {super.key,
-      required this.text,
-      required this.onTap,
-      this.leftIcon,
-      this.rightIcon});
+  const HafizhButtonWidget({
+    super.key,
+    required this.text,
+    required this.onTap,
+    this.disabled = false,
+    this.leftIcon,
+    this.rightIcon,
+  });
 
   @override
   Widget build(BuildContext context) {
     return ElevatedButton(
-      onPressed: onTap,
+      onPressed: () {
+        if (!disabled) {
+          onTap();
+        }
+      },
       style: ElevatedButton.styleFrom(
-        backgroundColor: context.colors.primary,
+        backgroundColor: disabled
+            ? context.colors.primary.withOpacity(0.5)
+            : context.colors.primary,
         foregroundColor: context.colors.onPrimary,
+        splashFactory: disabled ? NoSplash.splashFactory : null,
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(CircularConstant.lg),
         ),
-        padding: const EdgeInsets.symmetric(
-          vertical: 12,
-          horizontal: 16,
+        padding: EdgeInsets.symmetric(
+          vertical: 12.h,
+          horizontal: 16.w,
         ),
         shadowColor: Colors.transparent,
       ),
